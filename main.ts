dba382ec387fe20e3e64578733136c86f6226e3b
Samwel Omwenga <samwel.omwenga1@students.jkuat.ac.ke>
2024-01-08 19:02:44 +0300

    fix: add disconnect event to filter onlineUsers array when user logs out or disconnects

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import { Server } from "socket.io";
 type onlineUser = {
-    userId: string;
-    socketId: string;
-    };
-const io = new Server(5000,{
+  userId: string;
+  socketId: string;
+};
+const io = new Server(5000, {
   cors: {
     origin: "http://localhost:5173",
   },
@@ -11,14 +11,17 @@ const io = new Server(5000,{
 const onlineUsers: onlineUser[] = [];
 io.on("connection", (socket) => {
   console.log("socket id", socket.id);
-    socket.on("join", (userId:string) => {
-        !onlineUsers.some((user) => user.userId === userId) &&
-        onlineUsers.push({
+  socket.on("join", (userId: string) => {
+    !onlineUsers.some((user) => user.userId === userId) &&
+      onlineUsers.push({
         userId,
         socketId: socket.id,
-        });
-        console.log("onlineUsers", onlineUsers);
-    });
-
+      });
+    console.log("onlineUsers", onlineUsers);
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+  socket.on("disconnect", () => {
+    onlineUsers.filter((user) => user.socketId !== socket.id);
+    io.emit("getOnlineUsers", onlineUsers);
+  });
 });
-// io.listen(5000)
